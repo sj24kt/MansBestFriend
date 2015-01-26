f9f3cf31c92ac3ad438bbc4567a0140739d88776
Sherrie Jones <sj24kt@comcast.net>
2015-01-26 08:45:03 -0700

    Initial Commit

---
+++ MansBestFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MansBestFriend
+//
+//  Created by Sherrie Jones on 1/26/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
